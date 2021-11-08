d5d844d8beaa649a5a14a9a42b144e62479296a1
schuster19uk <schuster19uk@yahoo.co.uk>
2021-11-08 22:03:44 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const path = require("path");
+const sqlite3 = require("sqlite3").verbose();
+
+const app = express();
+
+// Express Configuration
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
+
+// Database Connection
+const db_name = path.join(__dirname, "data", "todo.db");
+const db = new sqlite3.Database(db_name, err => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Connected to database 'todo.db'");
+});
+
+
+// Task Table creation (unique_id, name, created_at, completed_at status)
+const sql_create = `CREATE TABLE IF NOT EXISTS Tasks (
+  unique_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name Text NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  completed_at DATETIME,
+  status VARCHAR(100) NOT NULL DEFAULT 'Pending'
+);`;
+
+//insert temp data
+db.run(sql_create, err => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Creation of table 'Tasks' successfull ");
+  // // Inserting Temp Data
+  // const sql_insert = `INSERT INTO Tasks (unique_id, name , status) VALUES
+  // (1, 'Task 1' , 'Pending'),
+  // (2, 'Task 2' , 'Pending'),
+  // (3, 'Task 3' , 'Pending');`;
+  // db.run(sql_insert, err => {
+  //   if (err) {
+  //     return console.error(err.message);
+  //   }
+  //   console.log("Insert of temp data 'Tasks' table was successfull ");
+  // });
+});
+
+require('../ToDoApp/routes/routes.js')(app , db);
+
+//Server Listen
+app.listen(3000, () => {
+    console.log("Server Listening on: (http://localhost:3000/) !");
+});
---
+++ routes/routes.js
@@ -0,0 +1,137 @@
+module.exports = function(app, db)
+{
+// GET /
+app.get("/", (req, res) => {
+    //res.render("index");
+    const sql = "SELECT * FROM Tasks ORDER BY unique_id";
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        return console.error(err.message);
+      }
+      res.render("Tasks", { model: rows });
+    });
+  
+  });
+  
+  // GET /Tasks
+  app.get("/Tasks", (req, res) => {
+    const sql = "SELECT * FROM Tasks ORDER BY unique_id";
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        return console.error(err.message);
+      }
+      res.render("Tasks", { model: rows });
+    });
+  });
+  
+  // GET /create
+  app.get("/create", (req, res) => {
+    res.render("create", { model: {} });
+  });
+  
+  // POST /create
+  app.post("/create", (req, res) => {
+  
+    const sql = "INSERT INTO Tasks (name) VALUES (?)";
+    const book = [req.body.name];
+    db.run(sql, book, err => {
+      if (err) {
+        return console.error(err.message);
+      }
+      res.redirect("/Tasks");
+    });
+  });
+  
+  
+  // POST /edit/5
+   app.post("/update/:id", (req, res) => {
+    const id = req.params.id;
+    const book = [id];
+    const sql = `UPDATE Tasks SET status = 'Completed' , completed_at = CURRENT_TIMESTAMP WHERE unique_id = ? and completed_at IS NULL` ;
+    db.run(sql, book , err => {
+      if (err) {
+        return console.error(err.message);
+      }
+      res.redirect("/Tasks");
+    });
+  }); 
+  
+  // GET /delete/5 (used to test delete)
+  app.get("/delete/:id", (req, res) => {
+    const id = req.params.id;
+    const sql = "SELECT * FROM Tasks WHERE unique_id = ?";
+    db.get(sql, id, (err, row) => {
+      if (err) {
+        return console.error(err.message);
+      }
+      res.render("delete", { model: row });
+    });
+  });
+  
+  // POST /delete/5
+  app.post("/delete/:id", (req, res) => {
+    const id = req.params.id;
+    const sql = "DELETE FROM Tasks WHERE unique_id = ?";
+    db.run(sql, id, err => {
+      if (err) {
+        return console.error(err.message);
+      }
+      res.redirect("/Tasks");
+    });
+  });
+
+  // GET /data
+  app.get("/data", (req, res) => {
+  
+
+    let toggle = "N"
+    let completedString = "";
+    if (req.query.completed != null)
+    {
+
+    toggle = req.query.completed;
+    }
+    
+    if(toggle == "N")
+    {
+      completedString = "Pending";
+    }
+    else
+    {
+      completedString = "Completed";
+    }
+
+    var date = new Date();
+    
+    //"2021-11-08 21:04:34"
+    var datestr =   date.toISOString().split('T')[0] + ' ' + date.toISOString().split('T')[1].split('.')[0];
+    
+    const searchValues = {
+        date: datestr ,
+        completed: completedString
+      };
+    
+      console.log("completed value is " + completedString);
+      console.log("date is " + searchValues.date);
+    
+    // date searches need to add date picker
+    //let sql = "SELECT * FROM Tasks  WHERE created_at <= date(?,'-2 day') and status = ? ORDER BY unique_id";
+    //let sql = "SELECT * FROM Tasks  WHERE created_at <= date(?) and status = ? ORDER BY unique_id";
+    
+    let sql = "SELECT * FROM Tasks WHERE created_at <= ? and status = ? ORDER BY unique_id";
+    
+      db.all(sql, [searchValues.date , searchValues.completed] , (err, rows) => {
+        console.log(sql)
+        if (err) {
+          return console.error(err.message);
+        }
+    
+        //console.log(rows)
+    
+        res.render("data", { search: searchValues , model: rows });
+      });
+      
+    
+    });
+
+}
